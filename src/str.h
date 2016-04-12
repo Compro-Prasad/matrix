f91df61135fab3b13cc8ad2bff488b89fa34b634
Compro-Prasad <compro.prasad@gmail.com>
2016-04-12 22:19:21 +0530

    Removed duplicate headers

--- src/str.h
+++
@@ -1,9 +0,0 @@
-#ifndef STR_H
-#define STR_H
-#include <stdio.h>
-
-void trim_cmd(char *cmd);
-void row_operation(DATATYPE **A, int mRows, int nCols, char *cmd);
-void col_operation(DATATYPE **A, int mRows, int nCols, char *cmd);
-
-#endif // STR_H
